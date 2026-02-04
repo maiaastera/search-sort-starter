2a0eb9d50c5562537c0cd9f8437a37ac7b6e4bd9
maiaastera <132973619+maiaastera@users.noreply.github.com>
2026-02-04 19:38:28 +0000

    done

--- script.js
+++ script.js
@@ -4,61 +4,64 @@ let NUM = Math.floor(Math.random() * 100);
 
 
 function sequentialSearch(){
+  let ans = "";
+  for(let i = 0; i <= 100; i++) {
+    if(NUM == i){
+      ans = i;
+    }
+  }
 
-  document.getElementById("output_sequential").innerHTML = "The number is....?";
-
+  document.getElementById("output_sequential").innerHTML = "The number is " + ans;
+  
 }
 
 function binarySearch() {
-  while(true) {
-
-    let guess = document.getElementById("guess").value;
-    document.getElementById("output_binary").innerHTML += guess;
-    console.log(guess)
-    console.log(NUM)
-
-    break;
+  let guess = document.getElementById("guess").value;
+  if(NUM == guess){
+    document.getElementById("output_binary").innerHTML += " The number is " + guess + "!!";
+  } else if(NUM >= guess){
+    document.getElementById("output_binary").innerHTML += " The number is higher than " + guess;
+  } else{
+    document.getElementById("output_binary").innerHTML += " The number is lower than " + guess;
   }
 }
 
 //sort test array using Bubble Sort
 function bubbleSort(arr) {
 
-document.getElementById("output_bubble").innerHTML = "before Bubble: " + arr
-/*  Bubble Sort pseudo code
-
-    for i from 0 to len
-      for j from 0 to len - i
-        if a[j] > a[j+1]
-          swap(a[j],a[j+1])
-*/    
-document.getElementById("output_bubble").innerHTML += "<br>after Bubble: " + arr
-
+  document.getElementById("output_bubble").innerHTML = "before Bubble: " + arr;
+  let save = "";
+  for(let i = 0; i <= 100; i++){
+    for(let j = 0; j <= 99; j++){
+      if(arr[j] > arr[j+1]){
+        save = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = save;
+      }
+    }
+  }
+  document.getElementById("output_bubble").innerHTML += "<br>after Bubble: " + arr;
 }
 
 //sort test array using selection sort
 function selectionSort(arr) {
-
+  let save = "";
   document.getElementById("output_selection").innerHTML = "<br>before Selection: " + arr
+  for(let i = 1; i < arr.length; i++){
+    let min = i;
+    for(let j = i+1; j < arr.length; j++){
+      if(arr[j] < arr[min]){
+        min = j;
+      }
+    }
+    if(min != i){
+      save = arr[i];
+      arr[i] = arr[min];
+      arr[min] = save;
+    }
+  }
 
-
-/*  Selection Sort pseudo code
-
-    for i from 1 to N - 1
-      // set current element as minimum
-      min = i    
-    
-      // check the element to be minimum 
-      for j from i+1 to N 
-        if a[j] < a[min]
-          min = j;
-    
-      //swap the minimum element with the current element
-      if min != i
-        swap(arr,a[min],a[i])
-*/ 
-
-  document.getElementById("output_selection").innerHTML += "<br>after Selection: " + arr
+  document.getElementById("output_selection").innerHTML += "<br>after Selection: " + arr;
 
 }
   
